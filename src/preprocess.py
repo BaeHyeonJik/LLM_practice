91b23abe3715ef5f6837310b9eadceb6e21e3dd9
BAE HYEONJIK <bhj4060@gmail.com>
2025-05-12 17:20:38 +0900

    feat: 파일 분리 로직 추가

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,11 +2,19 @@ import os
 import re
 
 
-def clean_text(folder_path):
+def clean_text(folder_path : str):
+  
+  # clean 폴더 생성
+  clean_folder_path = os.path.join(folder_path, "clean")
+  print(clean_folder_path)
+  if not os.path.exists(clean_folder_path):
+    os.makedirs(clean_folder_path)
 
-  for filename in os.listdir(folder_path):
+  origin_folder_path = os.path.join(folder_path, "origin")
+
+  for filename in os.listdir(origin_folder_path):
     if filename.endswith('.txt'):
-      file_path = os.path.join(folder_path, filename)
+      file_path = os.path.join(origin_folder_path, filename)
 
       # 파일 읽기
       with open(file_path, 'r', encoding='utf-8') as file:
@@ -16,8 +24,8 @@ def clean_text(folder_path):
       clean_text = re.sub(r'\n+', ' ', book_text)
       clean_text = re.sub(r'\s+', ' ', clean_text)
 
-      new_filename = os.path.join(folder_path, "cleaned_" + filename)
-
+      # 파일 쓰기
+      new_filename = os.path.join(clean_folder_path, "cleaned_" + filename)
       with open(new_filename, 'w', encoding='utf-8') as file:
         file.write(clean_text)
 
